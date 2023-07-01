932e0558d6d40c0a948387a6f4896ed62a99ce20
Brandon Wallace <brandonwallace403@gmail.com>
2023-07-01 13:02:18 -0400

    home.js changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import home from "../images/home.jpg";
 import "../styles/Home.css";
 
 function Home() {
   return (
-    <div className="home" style={{ backgroundImage: `url(${home})` }}>
+    <div className="home" style={{ backgroundImage: `url(https://assets.editorial.aetnd.com/uploads/2009/12/topic-july-4-gettyimages-815196336.jpg?)` }}>
       <div className="headerContainer">
-        <h1> I SELL THINGS </h1>
+        <h1>Lets celebrate our independence</h1>
         <Link to="/sale">
-          <button> Browse</button>
+          <button>Signees</button>
         </Link>
       </div>
     </div>
